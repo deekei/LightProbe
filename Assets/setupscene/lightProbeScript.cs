219174fd5d22e972ceda1c860a8fc2568a7d3e54
deekei <dakimstar@gmail.com>
2014-09-22 00:53:47 +1000

    Adding assets and updating gitignore

---
+++ Assets/setupscene/lightProbeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightProbeScript : MonoBehaviour {
+
+
+	public LightProbes lightProbeScene;
+
+	public float [] coefficientList;
+	// Use this for initialization
+	void Start () {
+
+		coefficientList = new float[27];
+
+
+		//lightProbeScene.GetInterpolatedLightProbe (this.transform.position, renderer, coefficientList);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LightmapSettings.lightProbes.GetInterpolatedLightProbe(this.transform.position, renderer, coefficientList);
+	}
+}
